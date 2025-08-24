11a55deb96a6b7f65656a781fc67f996151850cb
Eduarda <eduardaliz11@gmail.com>
2025-08-24 00:17:32 -0300

    Create variaveis.java

---
+++ variaveis.java
@@ -0,0 +1,21 @@
+// DECLARANDO VARIÁVEIS
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		//TIPO  NOME  = VALOR/CONTEUDO
+		String text = "Hello World! :)"; // string: texto, entre aspas.
+		int intNumber = 10 ; // int: números inteiros
+		double floatNumber = 10.5 ; // No Brasil usamos vírgula, no "mundo" é ponto!
+		//double também pode ser "float" de "número flutuante".
+		boolean booleanTrueOrFalse = true ; // recebe valor booleano
+		System.out.println(text);
+		System.out.println(intNumber);
+		System.out.println(floatNumber);
+		System.out.println(booleanTrueOrFalse);
+        intNumber = 15;
+        System.out.println(intNumber);
+        System.out.print(intNumber + 10);
+			}
+}
