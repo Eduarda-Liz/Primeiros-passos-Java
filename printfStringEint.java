bc074286356130c10499483ef8db4d27a284ddff
Eduarda <eduardaliz11@gmail.com>
2025-08-24 23:30:31 -0300

    Create printfStringEint.java

---
+++ printfStringEint.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int idade = 25;
+		String nome = "Duda"; //Lembrar de colocar aspas
+		System.out.printf("Eu sou %s e tenho %d anos", nome, idade); //o %s representa String e %d representa "decimal", queé numero inteiro
+	
+	}
+}
