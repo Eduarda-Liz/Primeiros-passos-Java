1f05ad69719a1edbe9c81e6ec6f2ae49b08787f4
Eduarda <eduardaliz11@gmail.com>
2025-08-27 15:03:13 -0300

    Create argumentos.java

---
+++ argumentos.java
@@ -0,0 +1,10 @@
+//ARGUMENTOS
+
+public class Main {
+
+	public static void main(String[] args) /*Dentro dos parentesis são parametros)*/{
+	  //A função padrão pode receber parametros.
+	  //String[] login //através de indexador, podemos acessar uma coleção
+	  System.out.println(args[]);//entre colchetes é índice
+	  //para o programa ficar dinamico, é possível passar indices para o args
+	  }
