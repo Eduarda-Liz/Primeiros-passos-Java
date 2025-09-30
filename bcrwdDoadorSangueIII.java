5d4797b44aa0fce516f7ce543351f71ddd096ec6
Eduarda <eduardaliz11@gmail.com>
2025-09-30 13:10:10 -0300

    Create bcrwdDoadorSangueIII.java

---
+++ bcrwdDoadorSangueIII.java
@@ -0,0 +1,20 @@
+/* O mundo está carente de solidariedade, e poucos atos são mais generosos do que doar sangue para salvar a vida de quem precisa.
+Porém, para realizar uma doação de sangue, existem alguns pré-requisitos.
+É necessário que o doador tenha entre 16 e 69 anos (inclusive), pese 50kg ou mais e tenha feito jejum.
+Para ajudar na campanha de doação de sangue de sua cidade, faça um programa que auxilie na validação dos requisitos para se tornar um doador de sangue. */
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		int idade = keyboard.nextInt();
+		double peso = keyboard.nextDouble();
+		String jejum = keyboard.next().toUpperCase();
+		
+		if (idade>=16 && idade<=69 && peso>=50 && jejum.equals("YES")) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
+	}
+}
